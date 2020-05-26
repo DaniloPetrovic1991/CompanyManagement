dc67ce8ee1e141267fbdca23b83ff884b767eac5
agrozdan93 <grozdanovicaleksandar93@gmail.com>
2020-05-26 19:52:29 +0200

    my first commit

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>About page IN NUXT</h1>
+</template>
+
+<script>
+export default {
+  data() {}
+};
+</script>
+
+<style>
+</style>
---
+++ pages/houses/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <p>houses</p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
